2c6c2c41327ba10cd2dd8b4aac040ef40114a326
Ana Carolina Evangelista da Costa <anacarolinaevangelistadacosta@gmail.com>
2025-07-22 18:02:00 -0300

    SwiftUI das telas principais

---
+++ PackRecipe/PackRecipe/Botões/BarraDeNome.swift
@@ -0,0 +1,18 @@
+//
+//  BarraDeNome.swift
+//  PackRecipe
+//
+//  Created by Ana Carolina Evangelista on 22/07/25.
+//
+
+import SwiftUI
+
+struct BarraDeNome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BarraDeNome()
+}
---
+++ PackRecipe/PackRecipe/Botões/botaoCheck.swift
@@ -0,0 +1,18 @@
+//
+//  botaoCheck.swift
+//  PackRecipe
+//
+//  Created by Ana Carolina Evangelista on 22/07/25.
+//
+
+import SwiftUI
+
+struct botaoCheck: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    botaoCheck()
+}
