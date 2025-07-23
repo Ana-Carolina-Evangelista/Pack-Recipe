f8970a366c9b5395d2e8c6f814a039b43781b91f
Ana Carolina Evangelista da Costa <anacarolinaevangelistadacosta@gmail.com>
2025-07-23 17:51:55 -0300

    Core Data e funcionalidade da tela 'criar mala'

---
+++ PackRecipe/PackRecipe/Core Data/CoreDataViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataViewModel.swift
+//  PackRecipe
+//
+//  Created by Ana Carolina Evangelista on 23/07/25.
+//
+
+import Foundation
---
+++ PackRecipe/PackRecipe/Core Data/CoredateController.swift
@@ -0,0 +1,8 @@
+//
+//  CoredateController.swift
+//  PackRecipe
+//
+//  Created by Ana Carolina Evangelista on 23/07/25.
+//
+
+import Foundation
