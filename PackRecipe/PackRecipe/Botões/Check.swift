3aed5152bcdc4bfbce57ab19c56c6c8676813ea3
Ana Carolina Evangelista da Costa <anacarolinaevangelistadacosta@gmail.com>
2025-07-29 17:26:15 -0300

    tentativa de resolução do botão check

---
+++ PackRecipe/PackRecipe/Botões/Check.swift
@@ -0,0 +1,18 @@
+//
+//  Check.swift
+//  PackRecipe
+//
+//  Created by Ana Carolina Evangelista on 29/07/25.
+//
+
+import SwiftUI
+
+struct Check: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Check()
+}
